8ef680f000035a30625e09e330e76de4b726faaa
ameng <1070534263@qq.com>
2023-03-16 17:22:56 +0800

    完毕，去看django了

--- flask_mysql/app.py
+++ flask_mysql/app.py
@@ -14,12 +14,18 @@ def add_user():
         pwd = request.form.get('pwd')
         mobile = request.form.get('mobile')
         #1. 连接数据库
-
+        conn = pymysql.connect(host="localhost",user="root",password="127956",database="flask_mysql")
+        cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
         #2. 执行SqL
-
+        sql = "insert into fm(username,password,number) values(%s,%s,%s)"
+        print(sql)
+        cursor.execute(sql,[user,pwd,mobile])
+        conn.commit()
         #3.关闭连接
-
+        cursor.close()
+        conn.close()
         return 'xxx'
 
 if __name__ == '__main__':
     app.run()
+    # print(int('123'))
